b7cb19387bf87c789ec12a3b3e8fe6ac708d807e
Ingo Düppe <ingo.dueppe@crowdcode.io>
2019-09-21 21:16:42 +0200

    addd deadlock simulator

--- src/main/java/io/crowdcode/jopt/joptbay/effects/DeadLock.java
+++ src/main/java/io/crowdcode/jopt/joptbay/effects/DeadLock.java
@@ -1,65 +1,62 @@
 package io.crowdcode.jopt.joptbay.effects;
 
-import java.util.concurrent.TimeUnit;
-
 public class DeadLock {
 
-    private Object monitorA = new Object();
-    private Object monitorB = new Object();
+	private Object monitorA = new Object();
+	private Object monitorB = new Object();
 
-    private volatile boolean stepA = false;
-    private volatile boolean stepB = false;
+	private volatile boolean stepA = false;
+	private volatile boolean stepB = false;
 
-    public void aThenB() {
-        synchronized (monitorA) {
-            System.out.println("A got a");
-            stepA = true;
-            while (!stepB) {
-                Thread.yield();
-            }
-            System.out.println("A trying b");
-            synchronized (monitorB) {
-                System.out.println("Got It!");
-            }
-        }
-    }
+	public static void main(String[] args) {
+		new DeadLock().buildDeadlock();
+	}
 
-    public void bThenA() {
-        synchronized (monitorB) {
-            System.out.println("B got b");
-            stepB = true;
-            System.out.println("B trying a");
-            synchronized (monitorA) {
-                System.out.println("Got It!");
-            }
-        }
-    }
+	public void aThenB() {
+		synchronized (monitorA) {
+			System.out.println("A got a");
+			stepA = true;
+			while (!stepB) {
+				Thread.yield();
+			}
+			System.out.println("A trying b");
+			synchronized (monitorB) {
+				System.out.println("Got It!");
+			}
+		}
+	}
 
-    public DeadLock buildDeadlock() {
-        ThreadGroup group = new ThreadGroup("Deadlock-Simulation");
-        Thread threadA = new Thread(group, this::aThenB, "AthenB");
-        Thread threadB = new Thread(group, this::bThenA, "BthenA");
+	public void bThenA() {
+		synchronized (monitorB) {
+			System.out.println("B got b");
+			stepB = true;
+			System.out.println("B trying a");
+			synchronized (monitorA) {
+				System.out.println("Got It!");
+			}
+		}
+	}
 
-        threadA.start();
-        Thread.yield();
-        while(!stepA) {
-            System.out.println("Main waiting for stepA");
-            Thread.yield();
-        }
-        System.out.println("Main starting B");
-        threadB.start();
-        while(!stepB) {
+	public DeadLock buildDeadlock() {
+		ThreadGroup group = new ThreadGroup("Deadlock-Simulation");
+		Thread threadA = new Thread(group, this::aThenB, "AthenB");
+		Thread threadB = new Thread(group, this::bThenA, "BthenA");
 
-            Thread.yield();
-        }
-        Thread.yield();
-        return this;
-    }
+		threadA.start();
+		Thread.yield();
+		while (!stepA) {
+			System.out.println("Main waiting for stepA");
+			Thread.yield();
+		}
+		System.out.println("Main starting B");
+		threadB.start();
+		while (!stepB) {
 
-    public static void main(String[] args) {
-        new DeadLock().buildDeadlock();
-    }
-
+			Thread.yield();
+		}
+		Thread.yield();
+		return this;
+	}
 
 
 }
